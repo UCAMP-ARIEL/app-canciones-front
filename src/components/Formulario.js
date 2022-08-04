e9b1a1305b41e0d8673be5538a502c6366b32850
rjoelgs <rjoelgs@hotmail.com>
2022-08-03 19:44:30 -0500

    request al backend

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
 import { Grid } from "@nextui-org/react";
 import ResultadosAlbum from "./ResultadosAlbum";
+import Alerta from "./Alerta";
+import axios from "axios";
 
 const Formulario = () => {
+  const [artista, setArtista] = useState("");
+  const [letra, setLetra] = useState("");
+  const [alerta, setAlerta] = useState("");
+
+  const buscarPorArtista = async () => {
+    if (!artista) {
+      return setAlerta("debes llenar el campo de artista");
+    }
+    setAlerta("");
+    //llamar a la API
+    try {
+      const respuesta = axios.get(
+        `http://localhost:4000/lyrics/get-songs?artist=${artista}`
+      );
+      console.log(respuesta);
+    } catch (error) {
+      console.log(error);
+    }
+
+    //resetear input
+    setArtista("");
+  };
+
+  const buscarPorLetra = async () => {
+    if (!letra) {
+      return setAlerta("debes llenar el campo de letra");
+    }
+    setAlerta("");
+    //llamar a la API
+    try {
+      const respuesta = axios.get(
+        `http://localhost:4000/lyrics/get-songs?title=${letra}`
+      );
+      console.log(respuesta);
+    } catch (error) {
+      console.log(error);
+    }
+    //resetear input
+    setArtista("");
+  };
+
   return (
     <Grid
       xs={12}
@@ -14,8 +57,19 @@ const Formulario = () => {
       <div
         style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
       >
-        <Input placeholder="Artista" />
-        <Button style={{ marginLeft: "15px" }} color="warning" auto>
+        <Input
+          aria-label="searchArtista"
+          onChange={(e) => setArtista(e.target.value)}
+          value={artista}
+          name="artista"
+          placeholder="Artista"
+        />
+        <Button
+          onClick={buscarPorArtista}
+          style={{ marginLeft: "15px" }}
+          color="warning"
+          auto
+        >
           buscar
         </Button>
       </div>
@@ -23,11 +77,24 @@ const Formulario = () => {
       <div
         style={{ display: "flex", justifyContent: "center", marginTop: "15px" }}
       >
-        <Input placeholder="Letra" />
-        <Button style={{ marginLeft: "15px" }} color="warning" auto>
+        <Input
+          aria-label="searchLetra"
+          onChange={(e) => setLetra(e.target.value)}
+          value={letra}
+          name="letra"
+          placeholder="Letra"
+        />
+        <Button
+          onClick={buscarPorLetra}
+          style={{ marginLeft: "15px" }}
+          color="warning"
+          auto
+        >
           buscar
         </Button>
       </div>
+
+      {alerta && <Alerta alerta={alerta} />}
 
       <ResultadosAlbum />
     </Grid>
